32144f94b4a56d84a88c1d127b22cc273d8d6c68
Hyeonjae Nam <jaemuon5582@gmail.com>
2025-02-02 03:42:00 +0900

    feat: font 추가

--- src/styles/globalStyles/globalStyles.ts
+++ src/styles/globalStyles/globalStyles.ts
@@ -1,11 +1,161 @@
 import { css } from '@emotion/react';
 
 const globalStyles = css`
+  @font-face {
+    font-family: 'NotoSansKR';
+    src: url('/assets/NotoSansKR-Thin.ttf') format('ttf');
+    font-style: normal;
+    font-weight: 100;
+  }
+
+  @font-face {
+    font-family: 'NotoSansKR';
+    src: url('/assets/NotoSansKR-Light.ttf') format('ttf');
+    font-style: normal;
+    font-weight: 300;
+  }
+
+  @font-face {
+    font-family: 'NotoSansKR';
+    src: url('/assets/NotoSansKR-Regular.ttf') format('ttf');
+    font-style: normal;
+    font-weight: 400;
+  }
+
+  @font-face {
+    font-family: 'NotoSansKR';
+    src: url('/assets/NotoSansKR-Medium.ttf') format('ttf');
+    font-style: normal;
+    font-weight: 500;
+  }
+
+  @font-face {
+    font-family: 'NotoSansKR';
+    src: url('/assets/NotoSansKR-Bold.ttf') format('ttf');
+    font-style: normal;
+    font-weight: 700;
+  }
+
+  @font-face {
+    font-family: 'NotoSansKR';
+    src: url('/assets/NotoSansKR-Black.ttf') format('ttf');
+    font-style: normal;
+    font-weight: 900;
+  }
+
+  @font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 100;
+    src: url('/assets/Inter-Thin.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Inter';
+    font-style: italic;
+    font-weight: 100;
+    src: url('/assets/Inter-ThinItalic.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 200;
+    src: url('/assets/Inter-ExtraLight.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Inter';
+    font-style: italic;
+    font-weight: 200;
+    src: url('/assets/Inter-ExtraLightItalic.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 300;
+    src: url('/assets/Inter-Light.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Inter';
+    font-style: italic;
+    font-weight: 300;
+    src: url('/assets/Inter-LightItalic.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    src: url('/assets/Inter-Regular.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Inter';
+    font-style: italic;
+    font-weight: 400;
+    src: url('/assets/Inter-Italic.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 500;
+    src: url('/assets/Inter-Medium.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Inter';
+    font-style: italic;
+    font-weight: 500;
+    src: url('/assets/Inter-MediumItalic.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 600;
+    src: url('/assets/Inter-SemiBold.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Inter';
+    font-style: italic;
+    font-weight: 600;
+    src: url('/assets/Inter-SemiBoldItalic.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 700;
+    src: url('/assets/Inter-Bold.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Inter';
+    font-style: italic;
+    font-weight: 700;
+    src: url('/assets/Inter-BoldItalic.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 800;
+    src: url('/assets/Inter-ExtraBold.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Inter';
+    font-style: italic;
+    font-weight: 800;
+    src: url('/assets/Inter-ExtraBoldItalic.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 900;
+    src: url('/assets/Inter-Black.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Inter';
+    font-style: italic;
+    font-weight: 900;
+    src: url('/assets/Inter-BlackItalic.woff2') format('woff2');
+  }
+
   * {
     margin: 0px;
     padding: 0px;
     box-sizing: border-box;
-    font-family: Pretendard;
   }
   li {
     list-style: none;
@@ -46,6 +196,21 @@ const globalStyles = css`
   }
   .app {
     position: relative;
+  }
+  body {
+    font-family:
+      'Inter',
+      'NotoSansKR',
+      'Helvetica Neue',
+      Helvetica,
+      Arial,
+      '맑은 고딕',
+      malgun gothic,
+      '돋움',
+      Dotum,
+      sans-serif,
+      'Apple Color Emoji',
+      'Noto Color Emoji';
   }
 `;
 
